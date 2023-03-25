484532b42193a1e6aeb4aee11d137d4bd403b231
blueplanet <blueplanet081@outlook.com>
2023-03-25 18:27:40 +0900

    試験用プログラム作成

---
+++ btest2.py
@@ -0,0 +1,17 @@
+'''
+    b_extension 試験用プログラム        2023/03/25 by te.
+'''
+import sys
+from b_expansion import b_expansion
+
+if len(sys.argv) > 1:
+    sdata = sys.argv[1]
+
+    items = b_expansion(sdata)
+
+    for it in items:
+        print(it)
+
+else:
+    print("b_extension 試験用プログラム")
+    print('usage: btest "試験文字列"')
